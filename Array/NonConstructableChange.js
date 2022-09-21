0c452b126bc023945c13c3666d0d801076019d3b
Cayla Bradley <99041331+caynoelani@users.noreply.github.com>
2022-09-21 13:31:52 -0500

    Three Number Sum Annotations

--- Array/NonConstructableChange.js
+++ Array/NonConstructableChange.js
@@ -1,13 +1,26 @@
+//Given an array of positive integers representing the values of coins in your posession, write a function that returns the minimum amount of change that you cannot create.
+
+//For example, if you are given coins = [1, 2, 5], the minimum amount of change that you cannot create is 4. If you are given no coins, you can't create 1
+
 function nonConstructibleChange(coins) {
     let currentSum = 0;
-    
-    coins = coins.sort((a, b) => a - b)
-    
+
+    coins = coins.sort(( a, b ) => a-b);
+    console.log(coins);
+
     for(let i = 0; i < coins.length; i++){
+
         if(coins[i] > currentSum + 1){
-            return currentSum + 1
+            return currentSum + 1;
+        }else {
+            currentSum += coins[i]
         }
-        currentSum += coins[i]
     }
     return currentSum + 1;
 }
+
+let coins1= [5, 7, 1, 1, 2, 3, 22] //20
+console.log(nonConstructibleChange(coins1))
+
+let coins2= [] //1
+console.log(nonConstructibleChange(coins2))
--- Array/ThreeNumberSum.js
+++ Array/ThreeNumberSum.js
@@ -1,25 +1,32 @@
-function threeNumberSum(array, targetSum) {
-    // Write your code here.
-    array.sort((a, b) => a - b)
-    let outputArr = []
+//Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. The function should find all triplets in an array that sum up to the target sum and return a 2D array of all these triplets. Each triplet should be ordered in ascending order, and the triplets themselves should be ordered in ascending order with respect to the numbers they hold. If no triplets, the function should return an empty array.
+
+function threeNumberSum(array, targetSum) { //takes in array of distinct integers and a target sum
+    let tripletArray = []; //tripletArray to track triplets that add to target
+
+    array = array.sort((a , b) => a - b); //sort array to iterate more effectively
+
+    for( let i = 0; i < array.length; i++){ //iterate through entire array
+        let start = i + 1; //start points to lowest possible value after array[i]
+        let end = array.length - 1; //end points to last index of array
+
+        while(start < end){ //start and end cannot equal or pass one another since we can't reuse digits
+            let tempTarget = targetSum - array[i]; //subtract array[i] from targetSum to convert problem into two number sum; a + b + c = target => a + b = target - c
     
-    for(let i = 0; i < array.length - 2; i++){
-    let j = i + 1
-    let k = array.length - 1
-    while(array[j] < array[k]){
-        let currentSum = array[i] + array[j] + array[k]
-        
-        if(currentSum === targetSum){
-        outputArr.push([array[i], array[j], array[k]])
-        j++
-        k--
-        } else if( currentSum < targetSum){
-        j++
-        } else{
-        k--
+            if(array[start] + array[end] === tempTarget){ //if valid triplet
+                tripletArray.push([array[i], array[start], array[end]]); //add triplet to output array
+                start++; //increment start position
+                end--; //decrement end position
+            }   else if(array[start] + array[end] > tempTarget){ //if greater than target
+                end--; //decrement end index
+            }   else { //if less than target
+                start ++; //increment start index
+            }
         }
     }
-    }
-    
-    return outputArr
+
+    return tripletArray; //return triplet array
 }
+
+let array1 = [12, 3, 1, 2, -6, 5, -8, 6]
+let target1 = 0
+console.log(threeNumberSum(array1, target1))
