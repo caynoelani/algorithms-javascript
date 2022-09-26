b3c15de35f8689ca8ea9c3f74c0f17a3756b55f2
Cayla Bradley <99041331+caynoelani@users.noreply.github.com>
2022-09-26 14:04:19 -0500

    Annotate MoveElementToEnd

--- Array/MoveElementToEnd.js
+++ Array/MoveElementToEnd.js
@@ -1,23 +1,23 @@
 //You're given an array of integers and an integer toMove. Write a function that moves all instances of that integer in the array to the end of the array and returns the array in place
 
-function moveElementToEnd(array, toMove) {
+function moveElementToEnd(array, toMove) { //takes in array and integer toMove
 
-    let j = array.length-1; 
-    let i = 0;
+    let j = array.length-1; //pointer starts at end of array
+    let i = 0; //pointer starts at beginning of array
 
-    while( i < j){
-        while(i < j && array[j] === toMove){
-            j--;
+    while( i < j){ //while j is greater than i
+        while(i < j && array[j] === toMove){ //while array[j] is equal to target integer and j is greater than i
+            j--; //decrement j
         }
 
-        if(array[i] === toMove){
-            [array[i], array[j]] = [array[j], array[i]]
+        if(array[i] === toMove){ //if array[i] equals target integer
+            [array[i], array[j]] = [array[j], array[i]] //swap values
         }
 
-        i++
+        i++ //increment i
     }
 
-    return array;
+    return array; //return input array
 }
 
 let array = [2, 1, 2, 2, 2, 3, 4, 2]
