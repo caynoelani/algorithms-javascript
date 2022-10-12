5a4f01df7bb2d37b40be2bd8eaf0df5896ea6905
Cayla Bradley <99041331+caynoelani@users.noreply.github.com>
2022-10-12 14:39:21 -0500

    Solve IterativeBSTClosestValue.js

--- Tree/IterativeBSTClosestValue.js
+++ Tree/IterativeBSTClosestValue.js
@@ -1,25 +1,21 @@
 //Write a function that takes in a Binary Search Tree (BST) and a target integer value and returns the closest value to that target value contained in the BST. You can assume that there will only be one closest value
 
-// function findClosestValueBST(tree, target){
-//     let currentNode = tree
-//     let returnValue = currentNode.value
-//     let currentDif = Infinity
-//     let tempDif
+function findClosestValueInBst(tree, target) {
+    let currentNode = tree
+    let closest = Infinity
 
-//     while(currentNode != null){
-//         tempDif = Math.abs(target - currentNode.value)
+    while(currentNode != null){
+        if(Math.abs(target - currentNode.value) < Math.abs(target - closest)){
+            closest = currentNode.value
+        }
 
-//         if(currentDif > tempDif){
-//             currentDif = tempDif
-//             returnValue = currentNode.value
-//         }
-
-//         if(currentNode.value > target){
-//             currentNode = currentNode.left
-//         } else if (currentNode.value < target){
-//             currentNode = currentNode.right
-//         } else {
-//             return currentNode.value
-//         }
-//     }
-// }
+        if(currentNode.value > target){
+            currentNode = currentNode.left
+        } else if (currentNode.value < target){
+            currentNode = currentNode.right
+        } else {
+            break
+        }
+    }
+    return closest
+}
